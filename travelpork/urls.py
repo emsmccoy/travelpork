6dbdebd4df91df1e2e14f4c5bc207b91330526b6
emma_alonso_mccoy <emmcoy7@gmail.com>
2025-09-05 17:10:08 +0200

    feat: create login workflow to different dashboards

--- travelpork/urls.py
+++ travelpork/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('redirect/', user_views.dashboard_redirect, name='dashboard_redirect'),
+    path('traveller-dashboard/', user_views.traveller_dashboard, name='traveller_dashboard'),
+    path('approver-dashboard/', user_views.approver_dashboard, name='approver_dashboard'),
+    path('default-dashboard/', user_views.default_dashboard, name='default_dashboard'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def dashboard_redirect(request):
+    if request.user.groups.filter(name='Approvers').exists():
+        return redirect('approver_dashboard')
+    elif request.user.groups.filter(name='Travellers').exists():
+        return redirect('traveller_dashboard')
+    else:
+        return redirect('default_dashboard')
+    
+@login_required
+def approver_dashboard(request):
+    return render(request, 'approver_dashboard.html')
+
+@login_required
+def traveller_dashboard(request):
+    return render(request, 'traveller_dashboard.html')
+
+@login_required
+def default_dashboard(request):
+    return render(request, 'default_dashboard.html')
