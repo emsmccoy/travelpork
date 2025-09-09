a161386454398395a85c0f8d2f7ec41bfb2de8f9
SimonMMB <menendezsimon@gmail.com>
2025-09-09 10:52:14 +0200

    fix: new approver's expense list template done

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('redirect/', views.dashboard_redirect, name='dashboard_redirect'),
     path('traveller-dashboard/', views.traveller_dashboard, name='traveller_dashboard'),
-    path('approver-dashboard/', views.approver_dashboard, name='approver_dashboard'),
     path('default-dashboard/', views.default_dashboard, name='default_dashboard'),
 ]
